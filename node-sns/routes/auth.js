6fc5e68eaf574fc86236e88bbb4165c7ff3ac546
jmjy1115 <108736255+jmjy1115@users.noreply.github.com>
2023-03-21 16:18:39 +0900

    comment out kakao login code
    comment out kakao login code

--- node-sns/routes/auth.js
+++ node-sns/routes/auth.js
@@ -17,13 +17,15 @@ router.get('/logout', isLoggedIn, logout);
 
 // GET /auth/kakao
 // 카카오 로그인 창으로 리다이렉트 => 성공 또는 실패 결과를 /auth/kakao/callback으로 보냄
-router.get('/kakao', passport.authenticate('kakao'));
+// router.get('/kakao', passport.authenticate('kakao'));
 
 // GET /auth/kakao/callback
+/*
 router.get('/kakao/callback', passport.authenticate('kakao', {
   failureRedirect: '/?loginError=카카오로그인 실패',
 }), (req, res) => { // 성공 시에는 /로 이동
   res.redirect('/');
 });
+*/
 
 module.exports = router;
