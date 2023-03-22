66a30648b8f1f421c33c674da1f549404521692b
HyunBaeJung <42843734+HyunBaeJung@users.noreply.github.com>
2023-03-22 14:49:30 +0900

    FE base

--- node-sns/react-project/src/App.js
+++ node-sns/react-project/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import kakaoImg from "./img/kakao.png";
+import googleImg from "./img/google.png";
+import naverImg from "./img/naver.png";
+
+function App() {  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="left"></div>
+    <div className="right">
+    <h2><span>스터디 호텔</span> <br/> STUDY AND REST</h2>
+     <div className="form">
+     
+
+      <form>
+          <input type="text" placeholder="아이디"/>
+          <input type="text" placeholder="비밀번호"/>
+          <div className='snsLogin'>
+            <img src={kakaoImg}/>
+            <img src={googleImg}/>
+            <img src={naverImg}/>
+          </div>
+          <button >회원가입</button> <button >로그인</button>
+      </form>
+      </div>
+    </div>
     </div>
   );
 }
