788285c289e42413217fdbcc7c3f1a708522c89c
HyunBaeJung <42843734+HyunBaeJung@users.noreply.github.com>
2023-04-02 12:07:14 +0900

    fe

--- web-server/hotel-fe/src/components/NavBar.js
+++ web-server/hotel-fe/src/components/NavBar.js
@@ -1,69 +1,64 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
-import logo from "../img/miniLogo-W.png";
-import { useCookies } from "react-cookie";
 
-export default function NavBar() {
-  function Profile() {
+export default function NavBar(sideColor, bgColor, setWindowHeight) {
 
-    const [cookies] = useCookies(["username"]);
+  const ButtonWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    height: 100%;
+    margin: 10px;
+    border-bottom: 1px;
+    border-color: black;
+  `;
 
-    return (
-      <div>
-        {cookies.username ? (
-          <ButtonWrapper>
-            <p>안녕하세요 {cookies.username}님</p>
-          </ButtonWrapper>
-        ) : (
-          <ButtonWrapper>
-            <Link to={"/SignUp"}>
-              <NavButton>회원가입</NavButton>
-            </Link>
-            <Link to={"/SignIn"}>
-              <NavButton>로그인</NavButton>
-            </Link>
-          </ButtonWrapper>
-        )}
-      </div>
-    );
+  const PlainButton = styled.button`
+    text-align: center;
+    border: 1px solid;
+    border-color: black;
+    width: 90px;
+    height: 25px;
+    font-size: 15px;
+    font-weight: 500;
+    color: ${sideColor};
+    border-radius: 10px;
+    border-color: ${sideColor};
+    margin-left: 8px;
+
+    &:hover {
+      background-color: ${bgColor};
+      color: #ffffff;
+    }
+  `;
+
+  function transWindow(event) {
+    if (event.target.id === "main-button") {
+      setWindowHeight("550px");
+    } else if (event.target.id === "board-button") {
+      setWindowHeight("600px");
+    } else if (event.target.id === "sign-up-button") {
+      setWindowHeight("620px");
+    } else if (event.target.id === "sign-in-button") {
+      setWindowHeight("500px");
+    }
   }
 
   return (
-    <NavBack>
-      <Logo src={logo} />
-      {Profile()}
-    </NavBack>
+    <div>
+      <ButtonWrapper>
+        <PlainButton id="main-button" onClick={transWindow}>
+          메인화면
+        </PlainButton>
+        <PlainButton id="board-button" onClick={transWindow}>
+          게시판
+        </PlainButton>
+        <PlainButton id="sign-up-button" onClick={transWindow}>
+          회원가입
+        </PlainButton>
+        <PlainButton id="sign-in-button" onClick={transWindow}>
+          로그인
+        </PlainButton>
+      </ButtonWrapper>
+    </div>
   );
 }
-
-const NavButton = styled.button`
-  height: 25px;
-  width: 60px;
-  border-radius: 4px;
-  border: 1px;
-  background-color: #dcd7c9;
-  font-size: 10px;
-  font-weight: 700;
-  margin: 2px;
-`;
-
-const ButtonWrapper = styled.div`
-  margin-right: 3px;
-`;
-
-const NavBack = styled.div`
-  background-color: #a27b5c;
-  height: 50px;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Logo = styled.img`
-  height: 40px;
-  width: auto;
-  padding-left: 10px;
-  padding-top: 3px;
-`;
--- web-server/hotel-fe/src/components/pages/SignUp/index.js
+++ web-server/hotel-fe/src/components/pages/SignUp/index.js
@@ -1,63 +1,7 @@
-import {React,useState} from "react";
-import CommonInput from "../../basic/CommonInput";
-import ImgInput from "../../basic/ImgInput";
-import BirthYearInput from "../../basic/BirthYearInput";
-import ComboBox from "../../basic/ComboBox";
+import React from 'react'
 
-import logo from "../../../img/logo.png"
-import pwImg from "../../../img/SignUp/pencil.png";
-import pwCheckImg from "../../../img/SignUp/check.png";
-import InputButtonCombo from "../../combination/InputButtonCombo";
-import InputBox from "../../basic/InputBox";
-import ButtonBox from "../../basic/ButtonBox";
-import Footer from "../../Footer";
-
-
-export default function SignUp() {
-
-  const [userId, setuserId] = useState("");
-  const [userPassword, setUserPassword] = useState("");
-  
-
+export default function index() {
   return (
-    <div className="h-screen bg-T1-beige font-naver">
-      <div className="flex justify-center items-center h-36">
-        <img style={ {width:"230px"}} alt="" src={logo}/>
-      </div>
-      <form className="flex flex-col items-center">
-        <CommonInput
-          title="아이디"
-          type="text"
-          id="id"
-          backPH="@studyH.com"
-        />
-        <ImgInput title="비밀번호" type="password" id="password" src={pwImg} />
-        <ImgInput
-          title="비밀번호 재확인"
-          type="password"
-          id="passwordCheck"
-          src={pwCheckImg}
-        />
-        <div className="h-3"></div>
-        <CommonInput title="이름" type="text" id="name" />
-        <BirthYearInput />
-        <ComboBox
-          title="성별"
-          contents={[
-            { face: "남자", value: "M" },
-            { face: "여자", value: "F" },
-            { face: "선택안함", value: "N" },
-          ]}
-          width="w-52"
-        />
-         <InputButtonCombo title="휴대전화" 
-         inputBox={ <InputBox type="number" id="phoneNumber" /> } 
-         buttonBox={<ButtonBox title="인증번호" width="w-16" bgColor="bg-T1-Brown"/>} />
-        <InputBox type="number" id="phoneNumber" width="w-52" readOnly={true} />
-        <ButtonBox width="w-52 mt-4" title="가입하기" bgColor="bg-T1-dark-green" />
-      </form>
-      
-      <Footer/>
-    </div>
-  );
+    <div>index</div>
+  )
 }
